c420aaad9bc41cf2915ef1b0b22b20dc3c30b3c3
energy1-914 <dlawldnjs5@daum.net>
2023-01-18 21:34:19 +0900

    feat: implement new posting feature

--- src/cores/api.js
+++ src/cores/api.js
@@ -1,16 +1,31 @@
-// api 요청 처리 파일
-import BASE_URL, { POSTS_URL } from "../../config.js";
-
+import { POSTS_URL, POST_URL } from "../../config.js";
 
 export class Api {
-  static async get(path) {
-    // const url = BASE_URL + `/${path}`;
+  static async get() {
     const url = POSTS_URL;
-
     const response = await fetch(url);
     const data = await response.json();
 
     if (response.ok) return data;
     else throw Error(data);
+  }
+
+  static async post(body) {
+    const url = POST_URL;
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    };
+    const response = await fetch(url, options);
+    const data = await response.text();
+
+    if (response.ok) {
+      // console.log("응답 정상");
+      return data;
+    } else {
+      // console.log("에러~!~!!~", Error(data));
+      throw Error(data)
+    }
   }
 }
--- src/pages/newpost.js
+++ src/pages/newpost.js
@@ -1,8 +1,9 @@
+import { Api } from "../cores/api";
 const layout = document.getElementById("layout");
 
 let template = `
 <div class="header">
-        <a href="">
+        <a href="#">
           <img
             class="arrowIcon"
             src="https://cdn-icons-png.flaticon.com/512/271/271220.png"
@@ -11,11 +12,13 @@ let template = `
         <h2>HPNY 2023</h2>
       </div>
       <main class="main">
-        <button class="newImg" href="">랜덤이미지 추가하기</button>
+      <form>
+        <button name="image" class="newImg" href="">랜덤이미지 추가하기</button>
         <div class="post">
           <h3>Title</h3>
           <input
-            class="title"
+            name="title"
+            class="inputTitle"
             type="text"
             placeholder="글 제목을 작성해주세요."
           />
@@ -23,11 +26,14 @@ let template = `
         <div class="post">
           <h3>Content</h3>
           <textarea
+            name= "content"
             class="content"
+            type="text"
             placeholder="글 내용을 작성해주세요."
           ></textarea>
         </div>
-        <button class="upload" href="">게시하기</button>
+        <button class="upload" type="submit" href="">게시하기</button>
+      </form>  
       </main>
 `;
 
@@ -35,7 +41,26 @@ export class NewPostView {
   static render() {
     layout.innerHTML = template;
     document.title = "새 포스트 작성";
-    document.querySelector("link").setAttribute("href", "src/css/newPost.scss"); //왜 안돼ㅜ
+    // document.querySelector("link").setAttribute("href", "src/css/newPost.scss");
+    this.upload();
+  }
+
+  static upload() {
+    const form = document.querySelector("form");
+    form.onsubmit = e => {
+      e.preventDefault();
+      let formData = new FormData(form);
+      let body = { image: "https://blog.kakaocdn.net/dn/Of181/btq4ID0fTeT/wftn2VI2aeYhGzarOLHn50/img.jpg" }; // image : 더미데이터
+      for (let [key, value] of [...formData]) {
+        body[key] = value;
+      }
+
+      console.log("formData: ", formData);
+      console.log("formData: ", [...formData]); // [['title', 'dd'], ['content', 'dd']]
+
+      Api.post(body)
+        .then(response => console.log("성공. post결과 : ", response))
+        .catch(error => console.log("에러 : ", error));
+    };
   }
 }
-
